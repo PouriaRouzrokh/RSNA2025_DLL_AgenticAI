d386a5eca00cd294b7eac4fc8669720e8a8f4a3a
Pouria Rouzrokh <po.rouzrokh@gmail.com>
2025-11-21 21:00:27 -0500

    Fix ViewSelector receiving incomplete niftiData
    - Pass full externalNiftiData to ViewSelector instead of niftiDataForRender
    - ViewSelector needs volume array for extractSlice to work properly
    - Fixes 'Cannot read properties of undefined' errors in extractSlice

--- frontend/src/components/viewer/CTViewer.jsx
+++ frontend/src/components/viewer/CTViewer.jsx
@@ -313,7 +313,7 @@ export default function CTViewer({
       <ViewSelector
         currentView={currentView}
         onViewChange={onViewChange}
-        niftiData={niftiDataForRender}
+        niftiData={externalNiftiData}
         windowLevel={windowLevel}
       />
     </div>
